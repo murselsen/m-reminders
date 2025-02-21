7f544350ca12360d62773e54b4c6620da8f03dc3
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-21 13:03:01 +0300

    feat(ui): remove console log from category add button

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ const categoryAddBtn = document.querySelector('#sidebarCategoryAddBtn');
 categoryAddBtn.addEventListener(
   'click',
   event => {
-    console.log(event.target.getAttribute('modal'));
+   
   },
   {
     passive: true,
