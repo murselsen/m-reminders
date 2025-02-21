55e1442121a4093023c636ad98d5d080fc1faeaa
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-21 14:36:33 +0300

    feat(index): render categories in index view

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.use("/public", express.static("public"));
-app.use("/node", express.static("node_modules"));
+const categories = require('./db/categories.json');
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.use('/public', express.static('public'));
+app.use('/node', express.static('node_modules'));
+
+app.get('/', (req, res) => {
+
+
+  res.render('index', { categories });
 });
 
 app.listen(port, () => {
