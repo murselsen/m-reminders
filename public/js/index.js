a3eb3f1f9928f1f150428856f2e466362e53c87b
Mürsel SEN <murselsen_35@hotmail.com>
2025-02-21 13:04:20 +0300

    fix(lightbox): Make lightbox full width

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ const categoryAddBtn = document.querySelector('#sidebarCategoryAddBtn');
 categoryAddBtn.addEventListener(
   'click',
   event => {
-   
+    document.querySelector(`#${event.target.getAttribute('modal')}`).style.display = 'block';
   },
   {
     passive: true,
