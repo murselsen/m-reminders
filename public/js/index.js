ee75b481e91a28f51c8ded993f331961a909c3fe
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-02 20:34:16 +0300

    fix(reminders): enhance reminder form interactivity and style

--- public/js/index.js
+++ public/js/index.js
@@ -8,3 +8,12 @@ categoryAddBtn.addEventListener(
     passive: true,
   },
 );
+
+const remidersAreaForm = document.querySelector('#remindersAreaForm');
+const remidersFormTitle = document.querySelector('#title');
+remidersFormTitle.addEventListener('input', e => {
+  console.log(e);
+  document.querySelector('#description').style.display = e.target.value.length > 0 ? 'block' : 'none';
+  document.querySelector('#date').style.display = e.target.value.length > 0 ? 'block' : 'none';
+  document.querySelector('#time').style.display = e.target.value.length > 0 ? 'block' : 'none';
+});
