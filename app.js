46e02045f96a0a815b7583f963411f54327855bc
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-03 02:19:54 +0300

    fix: update application version to 1.0.287 and enhance todoAddModal styles and functionality

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ app.use('/public', express.static('public'));
 app.use('/node', express.static('node_modules'));
 
 app.get('/', async (req, res) => {
+  const categories = await getCategories();
+  console.log(categories);
   res.render('index', {
-    categories: await getCategories(),
+    categories: categories,
   });
 });
 
