ebd110f32826189cdff24865e622fa0c6f35405c
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-03 04:44:03 +0300

    ``` No changes made ```

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,4 @@ import getTodos from './getTodos.js';
 import getTodosByCategory from './getTodosByCategory.js';
 import getCategory from './getCategory.js';
 // Exports the functions
-export { getTodos, getCategories, getTags, getTodosByCategory };
+export { getTodos, getCategories, getTags, getTodosByCategory, getCategory };
