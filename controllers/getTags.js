384e3d177aac3cd4ae2147912f8740fca60549ed
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-03 03:57:53 +0300

    fix: update application version to 1.0.460, export getCategories and getTags functions, and remove updateCategories script

--- controllers/getTags.js
+++ controllers/getTags.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 const getTags = async () => {
   let dbTodos = await fs.readFileSync(path.join(process.cwd(), 'db', 'todos.json'), 'utf8');
+  dbTodos = JSON.parse(dbTodos);
   console.log('Todo List: ', dbTodos);
 };
 
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 import getCategories from './getCategories.js';
 import getTags from './getTags.js';
 
+// Exports the functions
 export { getCategories, getTags };
