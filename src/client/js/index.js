f20807c24230cbc7e2caf3b432f4c84e56f1ab70
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-04 14:22:46 +0300

    Add new sidebar component, update images, and enhance project configuration; bump version to 1.0.73

---
+++ src/client/js/index.js
@@ -0,0 +1,17 @@
+/* const categoryAddBtn = document.querySelector('#sidebarCategoryAddBtn');
+categoryAddBtn.addEventListener(
+  'click',
+  event => {
+    document.querySelector(`#${event.target.getAttribute('modal')}`).style.display = 'block';
+  },
+  {
+    passive: true,
+  },
+);
+
+const todoAddModalActiveButton = document.querySelector('.form-input.title');
+todoAddModalActiveButton.addEventListener('click', e => {
+  document.querySelector('#todoAddFormModal').style.display = 'flex';
+});
+  */
+ 
