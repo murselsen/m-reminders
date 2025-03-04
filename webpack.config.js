770d0b5b482dbf175951365edc6c7e5ee7674226
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-04 11:23:19 +0300

    fix: update application version to 1.1.72, modify webpack configuration, and add Babel and CSS loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // Entry point of your application
+  entry: './public/js/index.js', // Entry point of your application
   output: {
     filename: 'bundle.js', // Output bundle file
-    path: path.resolve(__dirname, 'dist'), // Output directory
+    path: path.resolve(__dirname, 'public/dist'), // Output directory
   },
   module: {
     rules: [
       {
+        test: /\.js$/, // Apply this rule to JavaScript files
+        exclude: /node_modules/, // Exclude node_modules directory
+        use: {
+          loader: 'babel-loader', // Use Babel loader to transpile JavaScript
+        },
+      },
+      {
+        test: /\.css$/, // Apply this rule to CSS files
+        use: ['style-loader', 'css-loader'], // Use style-loader and css-loader
+      },
+    ],
   },
-  mode: 'development', //
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'), // Serve content from the dist directory
+    compress: true, // Enable gzip compression
+    port: 9000, // Port to run the development server
+  },
 };
