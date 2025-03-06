cf393c7fdbf4a3de36f724eb8c0c2c9fb22e9394
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-06 04:25:52 +0300

    Bump package version to 1.0.531; update package-lock and package.json

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -11,82 +11,29 @@ const renderReminders = () => {
       let reminders = res.data;
       console.log('Reminders: ', reminders);
 
-      reminders.forEach(reminder => {
-        const reminderItemHtmlObject = {
-          element: 'li',
-          id: `remTodo${reminder.id}`,
-          classList: ['reminder-item'],
-          children: [
-            {
-              element: 'div',
-              classList: ['remContent'],
-              children: [],
-            },
-            { ele},
-          ],
-        };
+      /*
+        const galleryMarkup = images
+  .map(
+    (image) => `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${image.original}">
+        <img
+          class="gallery-image"
+          src="${image.preview}"
+          data-source="${image.original}"
+          alt="${image.description}"
+        />
+      </a>
+    </li>
+  `
+  )
+  .join("");
 
-        // Reminder Item - Li
-        const reminderItemElement = document.createElement('li');
-        reminderItemElement.classList.add('reminder-item');
-        reminderItemElement.id = `remTodo${reminder.id}`;
+gallery.innerHTML = galleryMarkup; */
 
-        // Reminder Item Content - Div
-        const reminderItemContentElement = document.createElement('div');
-        reminderItemContentElement.classList.add('remContent');
+      const 
 
-        // Reminder Item Icon - Label
-        const reminderIconLabelElement = document.createElement('label');
-        reminderIconLabelElement.classList.add('remChechbox');
-        reminderIconLabelElement.setAttribute('for', `rem${reminder.id}`);
 
-        // Reminder Item Icon - i
-        const reminderIconItagElement = document.createElement('i');
-        reminderIconItagElement.classList.add('fa-regular', 'fa-2x');
-        reminderIconItagElement.id = `rem${reminder.id}Circle`;
-
-        const reminderIconInputElement = document.createElement('input');
-        reminderIconInputElement.type = 'checkbox';
-        reminderIconInputElement.name = 'completed';
-        reminderIconInputElement.id = `rem${reminder.id}`;
-        reminderIconInputElement.hidden = true;
-        reminderIconInputElement.addEventListener('change', e => {
-          reminderIconItagElement.classList.toggle('fa-circle');
-          reminderIconItagElement.classList.toggle('fa-circle-check');
-        });
-
-        if (reminder.completed) {
-          reminderIconItagElement.classList.add('fa-circle-check');
-          reminderIconItagElement.classList.remove('fa-circle');
-          reminderIconInputElement.checked = true;
-        } else {
-          reminderIconItagElement.classList.add('fa-circle');
-          reminderIconInputElement.checked = false;
-        }
-
-        reminderIconLabelElement.appendChild(reminderIconItagElement);
-        reminderIconLabelElement.appendChild(reminderIconInputElement);
-        reminderItemContentElement.appendChild(reminderIconLabelElement);
-
-        // Reminder Info - Div
-        const reminderItemContentInfoElement = document.createElement('div');
-        reminderItemContentInfoElement.classList.add('remInfo');
-
-        // Reminder Info Title - h4
-        const reminderItemContentInfoTitleElement =
-          document.createElement('h4');
-        reminderItemContentInfoTitleElement.classList.add('title');
-        reminderItemContentInfoTitleElement.textContent = reminder.title;
-
-        reminderItemContentInfoElement.appendChild(
-          reminderItemContentInfoTitleElement
-        );
-
-        reminderItemContentElement.appendChild(reminderItemContentInfoElement);
-        reminderItemElement.appendChild(reminderItemContentElement);
-
-        remindersList.appendChild(reminderItemElement);
-      });
     })
     .catch(err => {
       console.error('Error: ', err);
