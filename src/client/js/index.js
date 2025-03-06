e3133a711bac21ffc199424e7c616452fd23ed16
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-06 11:10:09 +0300

    Bump package version to 1.0.616; update package-lock and package.json, and implement tag rendering functionality in index.js

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -98,8 +98,6 @@ gallery.innerHTML = galleryMarkup; */
 };
 renderReminders();
 
-import axios from 'axios';
-
 const renderCategories = async () => {
   console.time('renderCategories');
   const sidebarCategoryList = document.querySelector('#sidebarCategoryList');
@@ -176,3 +174,34 @@ const renderCategories = async () => {
 
 // Categories
 renderCategories();
+
+const renderTags = async () => {
+  console.time('renderTags');
+  const sidebarTagList = document.querySelector('#sidebarTagList');
+  sidebarTagList.innerHTML = '';
+
+  axios('/api/tags').then(res => {
+    let tags = res.data;
+    for (let tag of tags) {
+      const tagItemElement = document.createElement('li');
+      tagItemElement.classList.add('tagItem');
+
+      const tagItemLinkElement = document.createElement('a');
+      tagItemLinkElement.classList.add('tagLink');
+      tagItemLinkElement.dataset.source = tag;
+      tagItemLinkElement.textContent = tag;
+
+      tagItemElement.appendChild(tagItemLinkElement);
+      sidebarTagList.appendChild(tagItemElement);
+    }
+  });
+  console.timeEnd('renderTags');
+};
+// Tags
+renderTags();
+document.querySelector('#sidebarTagList').addEventListener('click', e => {
+  if (e.target.nodeName === 'A') {
+    let source = e.target.dataset.source;
+    alert('Seçilen Etiket: ' + source.toString());
+  }
+});
