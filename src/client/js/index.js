e04724857fa5db68c6e50c6615c5ad90e3e5d738
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-06 11:09:40 +0300

    ``` Bump package version to 1.0.614; update package-lock and package.json, and optimize performance of tag rendering ```

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -97,3 +97,82 @@ gallery.innerHTML = galleryMarkup; */
   console.timeEnd('renderReminders');
 };
 renderReminders();
+
+import axios from 'axios';
+
+const renderCategories = async () => {
+  console.time('renderCategories');
+  const sidebarCategoryList = document.querySelector('#sidebarCategoryList');
+  sidebarCategoryList.innerHTML = '';
+
+  axios
+    .get('/api/categories')
+    .then(res => {
+      // console.log('Res: ', res.data);
+      let categories = res.data;
+      categories.forEach(category => {
+        const categoryItemElement = document.createElement('li');
+        categoryItemElement.classList.add('categoryItem');
+
+        // Category Icon - Div
+        const categoryIconDivElement = document.createElement('div');
+        categoryIconDivElement.classList.add('categoryItem-icon');
+        categoryIconDivElement.style.backgroundColor = category.color;
+        // Category Icon - Span
+        const categoryIconSpanElement = document.createElement('span');
+        categoryIconSpanElement.textContent = category.icon;
+        categoryIconDivElement.appendChild(categoryIconSpanElement);
+
+        categoryItemElement.appendChild(categoryIconDivElement);
+
+        // Category Text - P
+        const categoryTextElement = document.createElement('p');
+        categoryTextElement.classList.add('categoryItem-text');
+        categoryTextElement.textContent = category.title;
+
+        categoryItemElement.appendChild(categoryTextElement);
+
+        // Category Pin Icon - Button
+        const categoryPinElement = document.createElement('button');
+        categoryPinElement.classList.add('categoryItem-button');
+        categoryPinElement.id = `categoryItem${category.id}`;
+        categoryPinElement.dataset.source = category.id;
+
+        // Category Pin Icon - i
+        const categoryPinIconElement = document.createElement('i');
+        categoryPinIconElement.classList.add('fa-solid', 'fa-map-pin');
+
+        categoryPinElement.appendChild(categoryPinIconElement);
+        categoryItemElement.appendChild(categoryPinElement);
+
+        // Category Count - Span
+        const categoryCountElement = document.createElement('span');
+        categoryCountElement.classList.add('categoryItem-count');
+        categoryCountElement.textContent = category.count;
+
+        categoryItemElement.appendChild(categoryCountElement);
+        // Category List - Li added
+        sidebarCategoryList.appendChild(categoryItemElement);
+      });
+    })
+    .catch(err => {
+      console.error('Error: ', err);
+      const categoryItemElement = document.createElement('li');
+      categoryItemElement.classList.add('categoryItem');
+
+      // Category Text - P
+      const categoryTextElement = document.createElement('p');
+      categoryTextElement.classList.add('categoryItem-text');
+      categoryTextElement.style.color = 'red';
+      categoryTextElement.textContent = err.message;
+
+      categoryItemElement.appendChild(categoryTextElement);
+
+      sidebarCategoryList.appendChild(categoryItemElement);
+    });
+
+  console.timeEnd('renderCategories');
+};
+
+// Categories
+renderCategories();
