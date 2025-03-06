0210b480212391c2dec5475ac5ef456c789a0fe1
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-06 03:40:00 +0300

    Bump package version to 1.0.399; update package-lock and package.json, and modify renderReminders function to fetch reminders from the API

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -5,7 +5,12 @@ const renderReminders = () => {
   const remindersList = document.querySelector('#remindersList');
   remindersList.innerHTML = '';
 
-  axios.get('/')
+  axios
+    .get('/api/todos')
+    .then(res => {
+      
+    })
+    .catch(err => {});
 
   console.timeEnd('renderReminders');
 };
