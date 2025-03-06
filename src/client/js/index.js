69a19c25abab2b4690a75e2d49ee60955959c86e
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-06 03:49:11 +0300

    Bump package version to 1.0.430; update package-lock and package.json, fix reminder list ID in HTML, and enhance renderReminders function to display reminders from the API

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -3,14 +3,37 @@ import axios from 'axios';
 const renderReminders = () => {
   console.time('renderReminders');
   const remindersList = document.querySelector('#remindersList');
-  remindersList.innerHTML = '';
+  // remindersList.innerHTML = '';
 
   axios
     .get('/api/todos')
     .then(res => {
-      
+      let reminders = res.data;
+      console.log('Reminders: ', reminders);
+
+      reminders.forEach(reminder => {
+        // Reminder Item - Li
+        const reminderItemElement = document.createElement('li');
+        reminderItemElement.classList.add('reminder-item');
+        reminderItemElement.id = `remTodo${reminder.id}`;
+
+        // Reminder Item Content - Div
+        reminderItemContentElement = document.createElement('div');
+        reminderItemContentElement.classList.add('remContent');
+
+        // Reminder Item Icon - Label
+        const reminderIconElement = document.createElement('label');
+        reminderIconElement.classList.add('rem')
+
+        reminderItemElement.appendChild(reminderItemContentElement);
+
+        remindersList.appendChild(reminderItemElement);
+      });
     })
-    .catch(err => {});
+    .catch(err => {
+      console.error('Error: ', err);
+    });
 
   console.timeEnd('renderReminders');
 };
+renderReminders();
