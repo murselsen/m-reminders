360f8db9822e1cd6da763b8400b0f55709108a52
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-06 04:26:39 +0300

    Bump package version to 1.0.535; update package-lock and package.json, and enhance renderReminders function to improve reminder item structure and display additional information

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -31,9 +31,49 @@ const renderReminders = () => {
 
 gallery.innerHTML = galleryMarkup; */
 
-      const 
+      const remindersMarkup = reminders
+        .map(
+          reminder => `
+                    <li class="reminder-item" id="remTodo${reminder.id}">
+                      <div class="remContent">
+                        <label for="rem${reminder.id}" class="remCheckbox">
+                          <i class="fa-regular   fa-circle-check   fa-2x" id="rem${reminder.id}circle"></i>
+                          <input type="checkbox" name="complete" hidden id="rem${reminder.id}">
+                        </label>
+                        <div class="remInfo">
+                          <h4 class="title">
+                            {TodoTitle}
+                          </h4>
+                          <p class="description">
+                            {TodoDescrption}
+                          </p>
+                          <div class="infoBox">
 
+                            <span class="infoItem">
 
+                            </span>
+
+                          </div>
+                          <div class="tagBox">
+
+                            <span class="tagItem">
+                              {tag}
+                            </span>
+
+                          </div>
+                        </div>
+                        <div class="remActions">
+                          <button class="btn text-danger todo-del fa-solid fa-trash" data-source="0"></button>
+                          <button class="btn text-warning todo-edit fa-solid fa-pencil" data-source="0"></button>
+                        </div>
+                      </div>
+                      <div class="remSplit">
+                        <hr class="remSplitHr">
+                      </div>
+                    </li>
+        `
+        )
+        .join('');
     })
     .catch(err => {
       console.error('Error: ', err);
